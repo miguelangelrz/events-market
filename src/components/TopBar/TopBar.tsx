6a4c6a5e99639b407321cb9ff5e306970e11ba8f
scrollangel <migg.ro3@gmail.com>
2024-05-16 02:27:01 -0500

    Add typography to TopBar component
    The Typography component from "@mui/material" has been imported and added to the Toolbar of the TopBar component. This new element displays the text "Betbetbet" and creates a better alignment of items in the Toolbar.

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,4 +1,4 @@
-import { AppBar, IconButton, Toolbar } from "@mui/material";
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useState } from "react";
 import MarketSelectionsView from "../../views/DrawerViews/MarketSelections/MarketSelectionsView.tsx";
@@ -14,7 +14,10 @@ function TopBar() {
         anchor="right"
       />
       <AppBar position="static">
-        <Toolbar sx={{ display: "flex", justifyContent: "right" }}>
+        <Toolbar sx={{ display: "flex", justifyContent: "right", alignItems: "center" }}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Betbetbet
+          </Typography>
           <IconButton
             size="large"
             edge="start"
